e2dc7ec8973cfe0dd34f18f5c5735e5ce91ca959
tgkei <tagun1202@gmail.com>
2019-03-21 03:42:03 +0900

    15990 solved

--- by_python/week4/15990.py
+++ by_python/week4/15990.py
@@ -1,23 +1,37 @@
 N = int(input())
 
-cache = {}
-
-def f(target, avoid):
-    ret = 0
-    for new_avoid in range(1,4):
-        if new_avoid == avoid:
-            continue
-        
-        f(target-1,new_avoid)
-    return ret
-
-
+targets = []
 for _ in range(N):
-    ret = 0
-    target = int(input())
-    cur_score = 0
+    targets.append(int(input())-1)
 
-    for avoid in range(1,4):
-        ret = f(target, avoid)
+max_target = max(targets)
 
-    print(res%1000000009)
+cache = [[-1 for _ in range(100000)] for _ in range(3)]
+cache[0][0] = 0
+cache[0][1] = 1
+cache[0][2] = 2
+cache[1][0] = 1
+cache[1][1] = 0
+cache[1][2] = 2
+cache[2][0] = 1
+cache[2][1] = 1
+cache[2][2] = 2
+
+yirun = [1,1,3]
+
+ii = 2
+while ii < max_target:
+        ii += 1
+        cache[0][ii] = (cache[1][ii-2] + cache[2][ii-3]) % 1000000009
+        cache[1][ii] = (cache[0][ii-1] + cache[2][ii-3]) % 1000000009
+        cache[2][ii] = (cache[0][ii-1] + cache[1][ii-2]) % 1000000009
+
+for target in targets:
+    if target<=2:
+        ret = yirun[target]
+        print(ret)
+        continue
+    ret = 0    
+    ret += cache[0][target-1] + cache[1][target-2] + cache[2][target-3]
+
+    print(ret%1000000009)
