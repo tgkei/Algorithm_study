00980eb7b8759ed77d6aa8eb87ea5b592ff9c514
Taegyun Kim <tagun1202@gmail.com>
2020-08-31 19:25:44 +0900

    codenameb 3 problem solved (not checked)

--- by_python/codeb/c.py
+++ by_python/codeb/c.py
@@ -1,5 +1,6 @@
 from math import inf
 
+
 def find_smallest(pos, total_weight, weight_cache):
     global graph
     global cache
@@ -11,23 +12,28 @@ def find_smallest(pos, total_weight, weight_cache):
     if pos == N:
         return 0
 
-    if pos in weight_cache and (total_weight - weight_cache[pos]) < 0:
+    if pos in weight_cache:
         is_cycle = True
-        return inf
-    
+        if (total_weight - weight_cache[pos]) < 0:
+            return inf, True
+        return inf, False
+
     ret = inf
 
     if pos not in graph:
-        return inf
-    
+        return inf, False
+
     weight_cache[pos] = total_weight
     for n_pos, weight in graph[pos]:
-        ret = min(ret,weight+find_smallest(n_pos, total_weight+weight, weight_cache))
+        ret, is_impos = min(ret, weight+find_smallest(n_pos,
+                                                      total_weight+weight, weight_cache))
+        if is_impos:
+            return inf, is_impos
     weight_cache.pop(pos)
-    
-    cache[pos]= ret
-    return ret
-        
+
+    cache[pos] = ret
+    return ret, False
+
 
 graph = {}
 N, M = map(int, input().split())
@@ -37,15 +43,16 @@ is_cycle = False
 for _ in range(M):
     s, e, w = map(int, input().split())
     if s in graph:
-        graph[s].append((e,w))
+        graph[s].append((e, w))
     else:
-        graph[s] = [(e,w)]
+        graph[s] = [(e, w)]
 
 weight_cache = dict()
 
-res = find_smallest(1,0, weight_cache)
+res, is_impos = find_smallest(1, 0, weight_cache)
 
-if res == inf or is_cycle:
+if res == inf or is_impos:
     print("bug")
 else:
     print(res)
+
